64b6d513f1a096112791508330618eefa20e7add
Anton Zavyalov <anton.zavyalov@iqoption.com>
2019-08-27 12:11:55 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SCRIPT_PATH=$(dirname "$0")
+THEME_NAME=arc-night1
+
+cd $SCRIPT_PATH
+git submodule update --init
+
+./arc-theme/change_color.sh \
+	--output ${THEME_NAME} \
+	--autogen-opts '--disable-cinnamon --disable-gnome-shell \
+					--disable-transparency --disable-plank --disable-unity' \
+	./colors/arc-night
+
+echo "Done!"
